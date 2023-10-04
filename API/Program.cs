b372ecf5f2756d3f13bb618d75143b6b715ad7ae
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2023-10-04 21:32:10 +0700

    [FIX] Mifration and Seed

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(opt => {
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
 	opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
@@ -21,14 +22,31 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
+
 
 // app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try
+{
+	await context.Database.MigrateAsync();
+	await StoreContextSeed.SeedAsync(context);
+}
+catch (System.Exception e)
+{
+	logger.LogError(e, "An error occurred during migration");
+}
+
 
 app.Run();
