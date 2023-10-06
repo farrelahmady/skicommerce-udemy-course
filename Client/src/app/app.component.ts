671d468c9333cf49cdd69697a06853ed142f2cb6
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2023-10-06 22:18:03 +0700

    [FEAT] Http Client to Product API

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiCommerce';
+  products: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:8000/api/product').subscribe({
+      next: (res: any) => {
+        this.products = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete() {
+        console.log('complete');
+      },
+    });
+  }
 }
