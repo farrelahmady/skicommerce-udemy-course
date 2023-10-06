f08160987a51b36e6fee1c043695e0387d7fe97c
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2023-10-07 02:12:31 +0700

    [FIX] Filter and Sort Functionally

--- Client/src/app/shop/shop.component.ts
+++ Client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { ShopService } from './shop.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/product';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
 
 @Component({
   selector: 'app-shop',
@@ -9,16 +11,71 @@ import { Product } from '../shared/models/product';
 })
 export class ShopComponent implements OnInit {
   products: Product[] = [];
+  brands: Brand[] = [];
+  types: Type[] = [];
+  brandIdSelected = 0;
+  typeIdSelected = 0;
+  sortOptions = [
+    { name: 'Alphabetical', value: '' },
+    { name: 'Price: Low to High', value: 'priceAsc' },
+    { name: 'Price: High to Low', value: 'priceDesc' },
+  ];
+  sortSelected = '';
 
   constructor(private shopService: ShopService) {}
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService
+      .getProducts(this.brandIdSelected, this.typeIdSelected, this.sortSelected)
+      .subscribe({
+        next: (res) => {
+          this.products = res.data;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe({
       next: (res) => {
-        this.products = res.data;
+        this.brands = [{ id: 0, name: 'All' }, ...res];
       },
       error: (err) => {
         console.log(err);
       },
     });
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe({
+      next: (res) => {
+        this.types = [{ id: 0, name: 'All' }, ...res];
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  onBrandSelected(brandId: number) {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.typeIdSelected = typeId;
+    this.getProducts();
+  }
+
+  onSortSelected(event: any) {
+    this.sortSelected = event.target.value;
+    this.getProducts();
   }
 }
